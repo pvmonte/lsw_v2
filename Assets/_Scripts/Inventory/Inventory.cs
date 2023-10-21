3e23b40e733c0116de9959ba1accc44e655ec9e5
PauloMonte <paulovdecastro@gmail.com>
2023-10-21 20:59:45 -0300

    Inventory basics implemented

---
+++ Assets/_Scripts/Inventory/Inventory.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] private ItemSlot shirtSlot;
+    [SerializeField] private ItemSlot pantsSlot;
+    [SerializeField] private ItemSlot shoesSlot;
+    [SerializeField] private ItemSlot hairSlot;
+    [SerializeField] private ItemSlot hatSlot;
+    
+    [SerializeField] private List<Item> items;
+    
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+    }
+    
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+    }
+
+    public void EquipItem(Item item)
+    {
+        switch (item.Type)
+        {
+            case ItemType.Shirt:
+                shirtSlot.Equip(item);
+                break;
+            case ItemType.Pants:
+                pantsSlot.Equip(item);
+                break;
+            case ItemType.Shoes:
+                shoesSlot.Equip(item);
+                break;
+            case ItemType.Hair:
+                hairSlot.Equip(item);
+                break;
+            case ItemType.Hat:
+                hatSlot.Equip(item);
+                break;
+        }
+    }
+}
+
+[System.Serializable]
+public class ItemSlot
+{
+    [SerializeField] private GameObject equipped;
+    [SerializeField] private Transform slotTransform;
+
+    public void Equip(Item item)
+    {
+        if (equipped != null)
+        {
+            GameObject.Destroy(equipped.gameObject);
+        }
+        
+        GameObject.Instantiate(item, slotTransform);
+    }
+}
---
+++ Assets/_Scripts/Inventory/Item.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public enum ItemType
+{
+    Shirt,
+    Pants,
+    Shoes,
+    Hair,
+    Hat
+}
+
+[CreateAssetMenu(menuName = "Item", fileName = "Item")]
+public class Item : ScriptableObject
+{
+    [field: SerializeField] public ItemType Type { get; private set; }
+    [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public Sprite Icon { get; private set; }
+    [field: SerializeField] public GameObject Prefab { get; private set; }
+}
